c5b62fec0313c1cfe50c142758bfb15abde2c5da
Ильин Артём Михайлович <kot795689@gmail.com>
2026-09-25 20:59:53 +0300

    IZMENENIE

--- src/lab02/tuples.py
+++ src/lab02/tuples.py
@@ -1,16 +1,29 @@
 def format_record(rec: tuple[str, str, float]) -> str:
+    
     if type(rec) != tuple: #Проверка на кортеж 
         raise TypeError("Входные данные должны быть кортежем")
-    gpa = round(rec[2],2)
+    
+    gpa = round(rec[2],2) # Проверка на гпа
     if not (0.0 <= gpa <= 5.0):
         raise ValueError("GPA должен быть в диапазоне от 0.0 до 5.0")
-    fio = rec[0].split()
+    
+    fio = rec[0].split() # Проверка на имя
     if len(fio) == 0:
         raise ValueError('Напиши имя')
+    
+    if len(gr) == 0: # Поверка на группу
+        raise ValueError('Напиши группу')
+    
+    if len(rec) < 3: # Недополнение
+        raise ValueError("Неполный формат входных данных")
+    
+    if len(rec) > 3: # Переполнение
+        raise IndexError("Слишком много входных данных")
+    
     name = fio[0].lower() # Имя с строчных букв
     gr = rec[1]
-    if len(gr) == 0:
-        raise ValueError('Напиши группу')
+
+    
     if len(fio)== 3: 
         fio1 = f'{name[0].upper()+name[1:]} {fio[1][0].upper()}. {fio[2][0].upper()}.' # Сбор ФИО+группы+гпа
     else: 
